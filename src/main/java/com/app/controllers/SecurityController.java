76f62d3f5b6fc9d8492c8f99c44920b6b50cb062
maxx <gajevskij.maksim@gmail.com>
2021-07-29 21:22:27 +0300

    added basic authentication using spring security

---
+++ src/main/java/com/app/controllers/SecurityController.java
@@ -0,0 +1,18 @@
+package com.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/welcome")
+    public String getWelcomePage() {
+        return "welcome";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+}
